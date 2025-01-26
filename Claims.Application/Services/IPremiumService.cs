f800f0e7bacbf9208f94ff12b8f8a8f9ed2a2935
Tautvydas <tautvisv@gmail.com>
2025-01-26 03:48:56 +0200

    add demo documentation

--- Claims.Application/Services/IPremiumService.cs
+++ Claims.Application/Services/IPremiumService.cs
@@ -2,8 +2,18 @@
 
 namespace Claims.Application.Services
 {
+    /// <summary>
+    /// IPremiumService service computes premium prices
+    /// </summary>
     public interface IPremiumService
     {
+        /// <summary>
+        /// ComputePremiumAsync by perion duration and cover type
+        /// </summary>
+        /// <param name="startDate">Period start</param>
+        /// <param name="endDate">Period end</param>
+        /// <param name="coverType">Cover type which influence base price and discounts</param>
+        /// <returns></returns>
         Task<decimal> ComputePremiumAsync(DateOnly startDate, DateOnly endDate, CoverType coverType);
     }
 }
